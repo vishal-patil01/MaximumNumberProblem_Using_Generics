6e23393ce402e5510e8353c15dc1965659d030e6
Vishal Patil <vishalpatil01@hotmail.com>
2020-02-28 17:12:49 +0530

    REFACTORED : All 3 Methods To One Generic Method & Find Maximum

--- src/main/java/com/generics/MaximumNumberProblem.java
+++ src/main/java/com/generics/MaximumNumberProblem.java
@@ -1,8 +1,8 @@
 package com.generics;
 
 public class MaximumNumberProblem {
-    public Integer findMaximumNumber(Integer firstValue, Integer secondValue, Integer thirdValue) {
-        Integer maxValue = firstValue;
+   public <E extends Comparable <E>> E findMaximumNumber(E firstValue, E secondValue, E thirdValue) {
+       E maxValue = firstValue;
         if (maxValue.compareTo(secondValue) < 0) {
             maxValue = secondValue;
         }
@@ -11,24 +11,5 @@ public class MaximumNumberProblem {
         }
         return maxValue;
     }
-    public Float findMaximumNumber(Float firstValue, Float secondValue, Float thirdValue) {
-        Float maxValue = firstValue;
-        if (maxValue.compareTo(secondValue) < 0) {
-            maxValue = secondValue;
-        }
-        if (maxValue.compareTo(thirdValue) < 0) {
-            maxValue = thirdValue;
-        }
-        return maxValue;
-    }
-    public String findMaximumNumber(String firstValue, String secondValue, String thirdValue) {
-        String maxValue = firstValue;
-        if (maxValue.compareTo(secondValue) < 0) {
-            maxValue = secondValue;
-        }
-        if (maxValue.compareTo(thirdValue) < 0) {
-            maxValue = thirdValue;
-        }
-        return maxValue;
-    }
+
 }
