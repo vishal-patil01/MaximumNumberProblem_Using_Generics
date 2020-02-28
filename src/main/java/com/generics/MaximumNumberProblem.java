b07d1287a7b1654937281354088b04973deb6254
Vishal Patil <vishalpatil01@hotmail.com>
2020-02-28 17:59:14 +0530

    REFACTORED :Created Generic Class To Take 3 Variable Of Generic Type

--- src/main/java/com/generics/MaximumNumberProblem.java
+++ src/main/java/com/generics/MaximumNumberProblem.java
@@ -1,8 +1,23 @@
 package com.generics;
 
-public class MaximumNumberProblem {
-   public <E extends Comparable <E>> E findMaximumNumber(E firstValue, E secondValue, E thirdValue) {
-       E maxValue = firstValue;
+public class MaximumNumberProblem<E extends Comparable<E>> {
+
+    E firstValue;
+    E secondValue;
+    E thirdValue;
+
+    public MaximumNumberProblem(E firstValue, E secondValue, E thirdValue) {
+        this.firstValue = firstValue;
+        this.secondValue = secondValue;
+        this.thirdValue = thirdValue;
+    }
+
+    public E findMaximumNumber() {
+        return findMaximumNumber(firstValue, secondValue, thirdValue);
+    }
+
+    public E findMaximumNumber(E firstValue, E secondValue, E thirdValue) {
+        E maxValue = firstValue;
         if (maxValue.compareTo(secondValue) < 0) {
             maxValue = secondValue;
         }
@@ -11,5 +26,4 @@ public class MaximumNumberProblem {
         }
         return maxValue;
     }
-
 }
--- src/test/java/MaximumNumberTest.java
+++ src/test/java/MaximumNumberTest.java
@@ -1,58 +1,70 @@
 import com.generics.MaximumNumberProblem;
 import org.junit.Assert;
-import org.junit.Before;
 import org.junit.Test;
 
 public class MaximumNumberTest {
-    MaximumNumberProblem maximumNumberProblemObj;
-
-    @Before
-    public void setUp() throws Exception {
-        maximumNumberProblemObj = new MaximumNumberProblem();
-    }
 
     @Test
     public void givenIntegerMaxNumber_WhenAtFirstPosition_ShouldReturnSameNumber() {
-        Assert.assertSame(50, maximumNumberProblemObj.findMaximumNumber(50, 20, 30));
+        MaximumNumberProblem maximumNumberProblemObj = new MaximumNumberProblem(50, 20, 30);
+        Comparable maximumNumber = maximumNumberProblemObj.findMaximumNumber();
+        Assert.assertSame(50, maximumNumber);
     }
 
     @Test
     public void givenIntegerMaxNumber_WhenAtSecondPosition_ShouldReturnSameNumber() {
-        Assert.assertSame(50, maximumNumberProblemObj.findMaximumNumber(5, 50, 30));
+        MaximumNumberProblem maximumNumberProblemObj = new MaximumNumberProblem(50, 20, 30);
+        Comparable maximumNumber = maximumNumberProblemObj.findMaximumNumber();
+        Assert.assertSame(50, maximumNumber);
     }
 
     @Test
     public void givenIntegerMaxNumber_WhenAtThirdPosition_ShouldReturnSameNumber() {
-        Assert.assertSame(30, maximumNumberProblemObj.findMaximumNumber(10, 20, 30));
+        MaximumNumberProblem maximumNumberProblemObj = new MaximumNumberProblem(10, 20, 30);
+        Comparable maximumNumber = maximumNumberProblemObj.findMaximumNumber();
+        Assert.assertSame(30, maximumNumber);
     }
 
     @Test
     public void givenFloatMaxNumber_WhenAtFirstPosition_ShouldReturnSameNumber() {
-        Assert.assertEquals(50.2f, maximumNumberProblemObj.findMaximumNumber(50.2f, 20.2f, 30.2f), 0.0f);
+        MaximumNumberProblem maximumNumberProblemObj = new MaximumNumberProblem(50.2f, 20.2f, 30.2f);
+        Comparable maximumNumber = maximumNumberProblemObj.findMaximumNumber();
+        Assert.assertEquals(50.2f, (Float) maximumNumber, 0.0f);
     }
 
     @Test
     public void givenFloatMaxNumber_WhenAtSecondPosition_ShouldReturnSameNumber() {
-        Assert.assertEquals(50.2f, maximumNumberProblemObj.findMaximumNumber(5.2f, 50.2f, 30.2f), 0.0f);
+        MaximumNumberProblem maximumNumberProblemObj = new MaximumNumberProblem(50.2f, 20.2f, 30.2f);
+        Comparable maximumNumber = maximumNumberProblemObj.findMaximumNumber();
+        Assert.assertEquals(50.2f, (Float) maximumNumber, 0.0f);
     }
 
     @Test
     public void givenFloatMaxNumber_WhenAtThirdPosition_ShouldReturnSameNumber() {
-        Assert.assertEquals(30.2f, maximumNumberProblemObj.findMaximumNumber(10.2f, 20.2f, 30.2f), 0.0f);
+        MaximumNumberProblem maximumNumberProblemObj = new MaximumNumberProblem(50.2f, 20.2f, 30.2f);
+        Comparable maximumNumber = maximumNumberProblemObj.findMaximumNumber();
+        Assert.assertEquals(50.2f, (Float) maximumNumber, 0.0f);
     }
 
     @Test
     public void givenStringValue_WhenAtFirstPosition_ShouldReturnMaxStringValue() {
-        Assert.assertEquals("xyz", maximumNumberProblemObj.findMaximumNumber("xyz", "sam", "abc"));
+        MaximumNumberProblem maximumNumberProblemObj = new MaximumNumberProblem("xyz", "abc", "def");
+        Comparable maximumNumber = maximumNumberProblemObj.findMaximumNumber();
+        Assert.assertEquals("xyz", maximumNumber);
     }
 
     @Test
     public void givenStringValue_WhenAtSecondPosition_ShouldReturnMaxStringValue() {
-        Assert.assertEquals("xyz", maximumNumberProblemObj.findMaximumNumber("abc", "xyz", "sam"));
+        MaximumNumberProblem maximumNumberProblemObj = new MaximumNumberProblem("abc", "xyz", "def");
+        Comparable maximumNumber = maximumNumberProblemObj.findMaximumNumber();
+        Assert.assertEquals("xyz", maximumNumber);
     }
 
     @Test
     public void givenStringValue_WhenAtThirdPosition_ShouldReturnMaxStringValue() {
-        Assert.assertEquals("xyz", maximumNumberProblemObj.findMaximumNumber("abc", "sam", "xyz"));
+        MaximumNumberProblem maximumNumberProblemObj = new MaximumNumberProblem("abc", "def", "xyz");
+        Comparable maximumNumber = maximumNumberProblemObj.findMaximumNumber();
+        Assert.assertEquals("xyz", maximumNumber);
     }
+
 }
